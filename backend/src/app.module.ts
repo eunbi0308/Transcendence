513de133cc8e86bd2cf9646071b0175dfd690f98
eunbi-bb <eunbicho1995@gmail.com>
2024-10-07 19:19:35 +0200

    trying typeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { USER } from './database/USER/user.entity'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [USER],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
+// @Module({
+//   imports: [TypeOrmModule.forRoot(), UsersModule],
+// })
+
 export class AppModule {}
