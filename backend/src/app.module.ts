9e430bfd1b42d3b617eb15a1465349f876c2a887
rubenfin <rubenkfinnerud@gmail.com>
2024-10-09 13:45:03 +0200

    database and applications are connected.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,41 +1,4 @@
-// import { ConfigurableModuleBuilder, Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-// import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-// import { USER } from './database/USER/user.entity';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
-// import { UserService } from './database/USER/user.service';
 
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRoot({
-//       type: 'postgres',
-//       host: 'postgres',
-//       port: 5432,
-//       username: 'user',
-//       password: 'user123',
-//       database: 'postgres',
-//       entities: [USER],
-//       synchronize: true,
-//       autoLoadEntities: true
-//     }),
-//     TypeOrmModule.forFeature([USER], 'USER'),
-//     TypeOrmModule.forRootAsync({
-//       imports: [ConfigModule],
-//       useFactory: (configService: ConfigService) => ({
-//         type: 'postgres',
-//         host: 'postgres',
-//         port: 5432,
-//         username: configService.get('DB_USERNAME'),
-//         password: configService.get('DB_PASSWORD'),
-//         database: configService.get('DB_DATABASE_NAME'),
-//       }),
-//       inject: [ConfigService],
-//     })
-//   ],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
@@ -50,9 +13,9 @@ import { Controller } from '@nestjs/common';
       type: 'postgres',
       host: 'postgres',
       port: 5432,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE_NAME,
+      username: 'user',
+      password: 'user123',
+      database: 'postgres',
       entities: [],
       autoLoadEntities: true,
       synchronize: true,
--- backend/src/blocked/dto/update-blocked.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBlockedDto } from './create-blocked.dto';
-
-export class UpdateBlockedDto extends PartialType(CreateBlockedDto) {}
