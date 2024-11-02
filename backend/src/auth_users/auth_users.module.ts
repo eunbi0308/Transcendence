93b901f21fb6751e894593ad32a520ca3668322c
eunbi0308 <eunbicho1995@gmail.com>
2024-11-03 00:39:22 +0100

    ft-strategy.ts modified for passport-42

--- backend/src/auth_users/auth_users.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthUsersService } from './auth_users.service';
-
-@Module({
-  providers: [AuthUsersService],
-  exports: [AuthUsersService],
-})
-export class AuthUsersModule {}
