85b0541e6971ac4968fd82035d78982f62d3d272
Daan Balm <daanmlab@gmail.com>
2024-12-07 15:15:49 +0100

    Refactor updateUser function to use axios for user updates

--- frontend/src/pages/updateUser.tsx
+++ frontend/src/pages/updateUser.tsx
@@ -4,26 +4,22 @@ import Cookies from "universal-cookie";
 import axios from "axios";
 
 export default function UpdateUser() {
-    function updateUser(formData: FormData) {
+    async function updateUser(formData: FormData) {
         const nickname = formData.get("nickname");
         
-        fetch(`https://localhost:3000/users/me`, {
-            method: 'PATCH',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ nickname: nickname }),
-        }).then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        }).then(data => {
-            console.log('User updated successfully:', data);
-        }).catch(error => {
+        try {
+            const response = await axios(
+                {
+                    method: 'patch',
+                    url: 'https://localhost:3000/users/me',
+                    data: { nickname },
+                }
+            )
+            console.log('User updated successfully:', response.data);
+        } catch (error) {
             console.error('Failed to update user:', error);
-        });
+        }
+
     }
 
     return (
